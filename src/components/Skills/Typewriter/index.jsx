56745ae6245007511d1d71ce477170b541207193
gparin <kittikun.real@gmail.com>
2023-09-20 22:50:46 +0700

    refactor(skill page):add text typewritter animation

--- src/components/Skills/Typewriter/index.jsx
+++ src/components/Skills/Typewriter/index.jsx
@@ -4,8 +4,9 @@ const Typewriter = () => {
 
     return (
         <div class='typewriter'>
-            <h2 className='type-dev'>I'm Web Developer.</h2>
-            <h2 className='type-math'>I'm Mathematician.</h2>
+            <h2>Gparin | Kittikun.</h2>
+            <h2>I'm Web Developer.</h2>
+            <h2>I'm Mathematician.</h2>
         </div>
     )
 }
