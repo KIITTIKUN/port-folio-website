1d2bf435dfb11ec48c9f649d4900c8d1db1c3a27
gparin <kittikun.real@gmail.com>
2023-09-24 00:48:28 +0700

    fix(exp page): cover feedback mr. otto

--- src/components/Experience/Timeline/index.jsx
+++ src/components/Experience/Timeline/index.jsx
@@ -7,31 +7,29 @@ function Timeline() {
     <div className="timeline">
       <div className="white-dot left">
         <div className="content">
-          <h2>Since 2020</h2>
+          <h2>2020</h2>
           <p>
-          I've been learning Python for numerical 
-          methods and image processing with matrices.
+          I've learned Python for numerical methods and image processing with matrices and gained Knowledge involved in the technologies below
           </p>
           <ul>
-                <li>Python</li>
-                <li>Cv2</li>
+                <li>Python (fair)</li>
+                <li>Cv2 (little)</li>
             </ul>
         </div>
       </div>
       <div className="white-dot right">
         <div className="content">
-          <h2>Since 2021</h2>
+          <h2>2021</h2>
           <p>
-          I'm learning web development basics (HTML, CSS, JS, SQL) with FreeCodeCamp, focusing on responsive web design and algorithm structures.
+          I'm learning web development basics with FreeCodeCamp, focusing on responsive web design and algorithm structures, and gained Knowledge involved in the technologies below
           </p>
           <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>TypeScript</li>
-                    <li>JSON</li>
-                    <li>APIs</li>
-                    <li>SQL</li>
+              <li>HTML/CSS (experience)</li>
+              <li>JavaScript (experience)</li>
+              <li>TypeScript (fair)</li>
+              <li>JSON (experience)</li>
+              <li>Restful APIs (fair)</li>
+              <li>SQL (fair)</li>
                 </ul>
         </div>
       </div>
@@ -39,16 +37,16 @@ function Timeline() {
         <div className="content">
           <h2>Since 2022</h2>
           <p>
-          I participated in a cooperative program at Pow Solution as a Junior Web Developer for four months, gaining experience in Scrum, teamwork (using Clickup), GitHub collaboration, and developing programming skills in MVC, the MERN stack, and more.
+          I participated in a cooperative program at Pow Solution as a Junior Web Developer for 4 months, During this period, I gained experience in Scrum methodologies and collaboration using GitHub. I also honed my programming skills, with a focus on the MVC framework and the MERN stack. Gained Knowledge involved in the technologies below 
           </p>
           <ul>
-                    <li>React</li>
-                    <li>Express</li>
-                    <li>Mongo DB</li>
-                    <li>MySQL</li>
-                    <li>Node.js</li>
-                    <li>Github</li>
-                    <li>FPDF</li>
+                    <li>React (fair)</li>
+                    <li>Express (fair)</li>
+                    <li>Mongo DB (fair)</li>
+                    <li>Node.js (fair)</li>
+                    <li>Github (experience)</li>
+                    <li>Scrum methodologies</li>
+                    <li>PHP (fair)</li>
                 </ul>
         </div>
       </div>
@@ -56,23 +54,13 @@ function Timeline() {
         <div className="content">
           <h2>2023</h2>
           <p>
-          For my RMUTT mathematics bachelor's degree project, I'm developing a web application 
-          called 'Better AQI Estimator.' It uses Bilinear Interpolation method to display missing AQI 
-          data on a map using AQIcn APIs, helping users understand air quality at specific locations.
+          I applied mathematics with a web application by creating a project called 'Better Air Quality Index Estimator (Better AQI Estimator)' It uses the Bilinear Interpolation method to display missing AQI data on a map, helping users understand air quality at specific locations.
           </p>
           <ul>
-            <li>Latex</li>
-            <li>Leaflet.js</li>
-            <li>Jest</li>
+            <li>Latex (experience)</li>
+            <li>Leaflet.js (fair)</li>
+            <li>Jest (fair)</li>
           </ul>
-        </div>
-      </div>
-      <div className="white-dot left">
-        <div className="content">
-          <h2>2024</h2>
-          <p>
-          I would be highly honored if I could participate in writing my experiences in 2024 with your company
-          </p>
         </div>
       </div>
     </div>
