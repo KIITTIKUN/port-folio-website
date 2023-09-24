a11528b69262c6a8d5a22bf9e428ea52990a84bc
gparin <kittikun.real@gmail.com>
2023-09-24 15:24:05 +0700

    fix(Hamburger and home page): cover feedback mr.sarawut

--- src/components/Experience/Timeline/index.jsx
+++ src/components/Experience/Timeline/index.jsx
@@ -12,8 +12,8 @@ function Timeline() {
           I've learned Python for numerical methods and image processing with matrices and gained Knowledge involved in the technologies below
           </p>
           <ul>
-                <li>Python (fair)</li>
-                <li>Cv2 (little)</li>
+                <li>Python</li>
+                <li>Cv2</li>
             </ul>
         </div>
       </div>
@@ -24,29 +24,29 @@ function Timeline() {
           I'm learning web development basics with FreeCodeCamp, focusing on responsive web design and algorithm structures, and gained Knowledge involved in the technologies below
           </p>
           <ul>
-              <li>HTML/CSS (fair)</li>
-              <li>JavaScript (fair)</li>
-              <li>TypeScript (fair)</li>
-              <li>JSON (experience)</li>
-              <li>Restful APIs (fair)</li>
-              <li>SQL (fair)</li>
+              <li>HTML/CSS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>JSON</li>
+              <li>Restful APIs</li>
+              <li>SQL</li>
                 </ul>
         </div>
       </div>
       <div className="white-dot left">
         <div className="content">
-          <h2>Since 2022</h2>
+          <h2>2022</h2>
           <p>
           I participated in a cooperative program at Pow Solution as a Junior Web Developer for 4 months, During this period, I gained experience in Scrum methodologies and collaboration using GitHub. I also honed my programming skills, with a focus on the MVC framework and the MERN stack. Gained Knowledge involved in the technologies below 
           </p>
           <ul>
-                    <li>React (fair)</li>
-                    <li>Express (fair)</li>
-                    <li>Mongo DB (fair)</li>
-                    <li>Node.js (fair)</li>
-                    <li>Github (experience)</li>
+                    <li>React</li>
+                    <li>Express</li>
+                    <li>Mongo DB</li>
+                    <li>Node.js</li>
+                    <li>Github</li>
                     <li>Scrum methodologies</li>
-                    <li>PHP (fair)</li>
+                    <li>PHP</li>
                 </ul>
         </div>
       </div>
@@ -57,9 +57,9 @@ function Timeline() {
           I applied mathematics with a web application by creating a project called 'Better Air Quality Index Estimator (Better AQI Estimator)' It uses the Bilinear Interpolation method to display missing AQI data on a map, helping users understand air quality at specific locations.
           </p>
           <ul>
-            <li>Latex (experience)</li>
-            <li>Leaflet.js (fair)</li>
-            <li>Jest (fair)</li>
+            <li>Latex</li>
+            <li>Leaflet.js</li>
+            <li>Jest</li>
           </ul>
         </div>
       </div>
