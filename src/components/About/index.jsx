aa5cf53f66fa0b41a591795ae118c3f6955192f8
gparin <kittikun.real@gmail.com>
2023-09-24 15:47:26 +0700

    fix(about page): add university graduation

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -23,7 +23,8 @@ const About = () => {
             <h1>
                 <AnimateLetters stringArray={headerArray} index={15} letterClass={letterClass}/>
             </h1>
-            <p>I am Kittikun Parinyaprasert, a mathematics graduate in 2022 with the dream of becoming a web developer. I consistently strive to enhance my coding skills.</p>
+            <p>I am Kittikun Parinyaprasert, a mathematics graduate in 2022 at the Rajamangala University of Technology Thanyaburi. 
+                I dream of becoming a web developer. And I consistently strive to enhance my coding skills.</p>
             <p>I am seeking opportunities in the IT industry to leverage my skills and expertise on a variety of challenging projects.</p>
         </div>
         <div className="stage-cube-cont">
