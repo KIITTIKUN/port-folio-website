181fe34921376f0899a8381df7462a6fa4acb4fe
gparin <kittikun.real@gmail.com>
2023-10-09 20:48:25 +0700

    fix(about):year of grad

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <h1>
                 <AnimateLetters stringArray={headerArray} index={15} letterClass={letterClass}/>
             </h1>
-            <p>I'm Kittikun Parinyaprasert, a mathematics graduate in 2022 at the Rajamangala University of Technology Thanyaburi. 
+            <p>I'm Kittikun Parinyaprasert, a mathematics graduate in 2023 at the Rajamangala University of Technology Thanyaburi. 
                 I dream of becoming a web developer. And I consistently strive to enhance my coding skills.</p>
             <p>I'm seeking opportunities in the IT industry to leverage my skills and expertise on a variety of challenging projects.</p>
         </div>
